b96485b161e66c23f3b57d9d92532e9b677fad2d
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2023-04-17 07:33:09 +0200

    V1.3

--- search.js
+++ search.js
@@ -9,30 +9,30 @@ const rows = table.querySelectorAll("tr");
 searchForm.addEventListener("submit", function (event) {
     // prevent the default form submission behavior
     event.preventDefault();
-    
+
     // retrieve the search value and type
     const searchValue = searchInput.value;
     const searchType = searchSelect.value;
-  
+
     // iterate over each row in the table
     for (let i = 1; i < rows.length; i++) {
-      // retrieve the values for the current row
-      const level = rows[i].cells[3].textContent.toLowerCase();
-      const gpa = rows[i].cells[4].textContent.toLowerCase();
-      const department = rows[i].cells[9].textContent.toUpperCase();
-      const id = rows[i].querySelectorAll("td")[2].textContent;
-      
-      // check if the current row matches the search criteria
-      if (searchType === "ID" && id.includes(searchValue)) {
-        rows[i].style.display = "";
-      } else if(searchType === "Level" && level.includes(searchValue)) {
-        rows[i].style.display = "";
-      } else if(searchType === "GPA" && gpa.includes(searchValue)) {
-        rows[i].style.display = "";
-      } else if(searchType === "Departement" && department.includes(searchValue)) {
-        rows[i].style.display = "";
-      } else {
-        rows[i].style.display = "none";
-      }
+        // retrieve the values for the current row
+        const level = rows[i].cells[3].textContent.toLowerCase();
+        const gpa = rows[i].cells[4].textContent.toLowerCase();
+        const department = rows[i].cells[9].textContent.toUpperCase();
+        const id = rows[i].querySelectorAll("td")[2].textContent;
+
+        // check if the current row matches the search criteria
+        if (searchType === "ID" && id.includes(searchValue)) {
+            rows[i].style.display = "";
+        } else if (searchType === "Level" && level.includes(searchValue)) {
+            rows[i].style.display = "";
+        } else if (searchType === "GPA" && gpa.includes(searchValue)) {
+            rows[i].style.display = "";
+        } else if (searchType === "Departement" && department.includes(searchValue)) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
     }
-  });
+});
