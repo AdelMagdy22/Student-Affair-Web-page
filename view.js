c7c5106a66c591788e1eb013c531122f4675f81a
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2023-04-17 14:25:02 +0200

    V1.0

---
+++ view.js
@@ -0,0 +1,42 @@
+// Wait for the document to load before attaching event listeners
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Select the drop-down element
+    var selectStatue = document.querySelector('#selectStatue');
+
+    // Attach an event listener to the drop-down element
+    selectStatue.addEventListener('change', function () {
+
+        // Select all the rows in the table body
+        var rows = document.querySelectorAll('tbody tr');
+
+        // If "All Students" is selected, show all rows
+        if (selectStatue.value === 'All Students') {
+            for (var i = 0; i < rows.length; i++) {
+                rows[i].style.display = '';
+            }
+        }
+        // If "Active" is selected, show only rows with "active" status
+        else if (selectStatue.value === 'Active') {
+            for (var i = 0; i < rows.length; i++) {
+                var status = rows[i].querySelector('td:nth-child(9)').textContent;
+                if (status.toLowerCase() === 'active') {
+                    rows[i].style.display = '';
+                } else {
+                    rows[i].style.display = 'none';
+                }
+            }
+        }
+        // If "Inactive" is selected, show only rows with "inactive" status
+        else if (selectStatue.value === 'InActive') {
+            for (var i = 0; i < rows.length; i++) {
+                var status = rows[i].querySelector('td:nth-child(9)').textContent;
+                if (status.toLowerCase() === 'inactive') {
+                    rows[i].style.display = '';
+                } else {
+                    rows[i].style.display = 'none';
+                }
+            }
+        }
+    });
+});
